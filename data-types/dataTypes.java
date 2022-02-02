38d81eca0cb27daa0f4c6b9a95e3292b844b73de
Pallavi <pallavi0102.cse19@chitkara.edu.in>
2022-02-02 17:46:00 +0530

    first commit

---
+++ data-types/dataTypes.java
@@ -0,0 +1,26 @@
+public class dataTypes {
+
+    public static void main(String[] args) {
+        String a = "pallavi";
+        int num = 2, x=999999, y=-600; //whole nums
+
+        //double lonf float
+        long p = 99999999L;  //suffixed by L
+        float f = 233333.5F; //suffixed by F
+        double d = 33333333333333.7; //no suffix, bigger range than float
+
+        //boolean
+        boolean b = false; //only be true or false not any num; no double quotes;
+
+        System.out.println(a);
+        System.out.println(num);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(p);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+
+    }
+
+}
