37f31eba9a92721adf3c4e113acae07f7a1b8de7
Pallavi <pallavi0102.cse19@chitkara.edu.in>
2023-03-20 19:42:46 +0530

    try-catch

--- exceptions/exceptions.java
+++ exceptions/exceptions.java
@@ -12,5 +12,25 @@ public class exceptions {
 
          Scanner scan = null;   //no error as non-primitive
          scan.nextInt();        // throws null pointer exception as scan points to nothing(null) so we can't call a function on it
+
+
+        /*** Handling Exceptions ***/
+
+        /*Exceptions can be handled using try(try this code) and catch(run this code if try throws an exception i.e. catch the exception). Using try-catch, we can also specify the code block to be run in case of different exceptions(errors) */
+
+        try{
+            int[] var = {4};
+            System.out.println(var[1]);    //will throw an exception
+        }
+        catch(NullPointerException e) {
+            System.out.println("Your array is null!");
+        }        
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index is out of bounds");
+        }
+        catch(Exception e) {
+            System.out.println("Something else went wrong!");
+        }
+
     }
 }
